55034c958e0a76726fba6643137318d43423d83c
poteto <naonyan1135@gmail.com>
2024-09-06 01:58:42 +0900

    comment

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 import { createApp } from "vue"
-import App from "./App.vue" //* 重要
+import App from "./App.vue" //* 重要 default importできるようにviteが勝手にしてくれる
 
 createApp(App) //* createAppは関数 引数にコンポーネントを渡す必要あり
