e6d911064e2e73793951add56b8bb1f104adf153
poteto <naonyan1135@gmail.com>
2024-09-08 01:05:47 +0900

    reactive

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
-import { ref } from "vue" //* refは関数 使うときはデータをオブジェクトにする必要がある
+import { ref, reactive } from "vue" //* refは関数 使うときはデータをオブジェクトにする必要がある
 const title = "Vue.js Course"
 let price = ref(9.99) //* refに理アクティビティにしたいものを渡す
 //* refが返す複雑なオブジェクトはrefオブジェクトっていう名前
 // console.log(price.value) //* .valueっていうプロパティがある
 const increment = () => {
   price.value += 1
+  instructor.age += 1
   // console.log(price)
 }
 
@@ -14,6 +15,13 @@ const info = ref({
   rating: 4
 })
 console.log(info.value.students)
+
+const instructor = reactive({
+  //* オブジェクトがそのまま使えるように見えるようになる
+  name: "Yoshipi",
+  age: 25
+})
+console.log(instructor.age) //* これで呼べちゃう
 </script>
 
 <template>
@@ -22,6 +30,7 @@ console.log(info.value.students)
   <!-- こっちは.valueをつけちゃだめ 自動的にref objectかどうかをチェックしてるから -->
   <!--普通にjsの式を入れられて結果が表示される ejsとかと一緒 -->
   <h3>Stundents: {{ info.students }}</h3>
+  <h3>instructor: {{ instructor.age }}</h3>
   <button @click="increment">button</button>
   <!-- @イベント名="関数名"でイベント時に実行される -->
 </template>
