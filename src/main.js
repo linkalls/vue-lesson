666c7ceb185683c24a7a1ff768fa3c19a040a51e
poteto <naonyan1135@gmail.com>
2024-09-08 00:31:04 +0900

    add

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from "vue"
 import App from "./App.vue" //* 重要 default importできるようにviteが勝手にしてくれる
 
-createApp(App) //* createAppは関数 引数にコンポーネントを渡す必要あり
+const app = createApp(App) //* createAppは関数 引数にコンポーネントを渡す必要あり UIを作るだけで表示はしないから書かなきゃいけない 返り値を返してる
+app.mount("#app") //* メソッドを使って表示 引数にcss selectorと同じルールで場所を指定
