aff7b097c0c5d2e9ad5850769e36ed9a0e5395d8
poteto <naonyan1135@gmail.com>
2024-09-08 01:00:43 +0900

    これじゃ二度手間

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,29 @@
 <script setup>
-import {ref} from "vue" //* refは関数 使うときはデータをオブジェクトにする必要がある
+import { ref } from "vue" //* refは関数 使うときはデータをオブジェクトにする必要がある
 const title = "Vue.js Course"
 let price = ref(9.99) //* refに理アクティビティにしたいものを渡す
 //* refが返す複雑なオブジェクトはrefオブジェクトっていう名前
-console.log(price.value)  //* .valueっていうプロパティがある
-const increment = () =>{
+// console.log(price.value) //* .valueっていうプロパティがある
+const increment = () => {
   price.value += 1
-console.log(price)
+  // console.log(price)
 }
+
+const info = ref({
+  students: 1000,
+  rating: 4
+})
+console.log(info.value.students)
 </script>
 
 <template>
   <h1>{{ title }}</h1>
-   <h2>${{ price -1 }}</h2> 
-   <!--普通にjsの式を入れられて結果が表示される ejsとかと一緒 -->
-   <button @click="increment">button</button> <!-- @イベント名="関数名"でイベント時に実行される -->
+  <h2>${{ price - 1 }}</h2>
+  <!-- こっちは.valueをつけちゃだめ 自動的にref objectかどうかをチェックしてるから -->
+  <!--普通にjsの式を入れられて結果が表示される ejsとかと一緒 -->
+  <h3>Stundents: {{ info.students }}</h3>
+  <button @click="increment">button</button>
+  <!-- @イベント名="関数名"でイベント時に実行される -->
 </template>
 
 <style>
