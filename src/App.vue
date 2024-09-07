9764c5c65da4ffcddec22d45b7c4a084dc1ae57d
poteto <naonyan1135@gmail.com>
2024-09-08 00:44:16 +0900

    @click

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <script setup>
 const title = "Vue.js Course"
 let price = 9.99
+const increment = () =>{
+console.log("click")
+}
 </script>
 
 <template>
   <h1>{{ title }}</h1>
    <h2>${{ price -1 }}</h2> 
    <!--普通にjsの式を入れられて結果が表示される ejsとかと一緒 -->
+   <button @click="increment">button</button> <!-- @イベント名="関数名"でイベント時に実行される -->
 </template>
 
 <style>
