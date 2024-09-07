5efe40499eb4c08add3f010f20f882299002ddb7
poteto <naonyan1135@gmail.com>
2024-09-08 00:41:36 +0900

    面白いね

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <script setup>
 const title = "Vue.js Course"
+let price = 9.99
 </script>
 
 <template>
-  <h1>{{title}}</h1>
+  <h1>{{ title }}</h1>
+   <h2>${{ price -1 }}</h2> 
+   <!--普通にjsの式を入れられて結果が表示される ejsとかと一緒 -->
 </template>
 
 <style>
 h1 {
-  color: red;
+  color: skyblue;
 }
 </style>
